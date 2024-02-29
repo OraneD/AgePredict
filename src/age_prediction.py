dce783a5587c5a902f9e95ee58d1a89908211920
Orane Dufour <orane.dufour@gmail.com>
2024-02-29 17:26:55 +0100

    correction chemin modèle

--- src/age_prediction.py
+++ src/age_prediction.py
@@ -11,11 +11,11 @@ import numpy as np
 from tensorflow.keras.models import load_model
 
 """
-Chargment des modèles en fonction du sexe et prédiction de l'âge
+Chargement des modèles en fonction du sexe et prédiction de l'âge
 """
 
 def predict_age(file,sexe):
-    model_path = "../modeles/best_women_24loc.h5" if sexe == "femme" else "..modeles/best_men_24loc.h5"
+    model_path = "../modeles/best_women_24loc.h5" if sexe == "femme" else "../modeles/best_men_24loc.h5"
     model = load_model(model_path) 
     predictions = model.predict(extract_vectors(file,sexe))
     predictions = np.argmax(predictions, axis = 1)
